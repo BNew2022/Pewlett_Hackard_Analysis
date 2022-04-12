cd5826038df030dc39e30ed5cd5fb56236ee8e04
BNew2022 <brentnewman223@gmail.com>
2022-04-12 11:17:30 -0400

    queries

--- schema.sql
+++ schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE employees(
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -37,11 +37,10 @@ CREATE TABLE salaries (
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
-  salary INT NOT NULL,
+  title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE dept_emp(
